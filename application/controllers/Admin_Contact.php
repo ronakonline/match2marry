150709eb23c3981e81968ad23f1384cc707d96c1
YASH <daiyayash1218@gmail.com>
2020-02-09 19:55:47 +0530

    contact information done

--- application/controllers/Admin_Contact.php
+++ application/controllers/Admin_Contact.php
@@ -4,9 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Admin_Contact extends CI_Controller{
 
     public function index(){
-
+    	
         if(isset($_SESSION['user'])){
-            $this->load->view('admin/contact');
+            $this->load->model('admin_contact_m');
+            $data = $this->admin_contact_m->getdetails();
+            $this->load->view('admin/contact',array('my_data'=>$data));
         }else{
             redirect(base_url().'Admin_login');
         }
