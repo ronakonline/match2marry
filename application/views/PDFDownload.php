e060399f0f4e29b0266875b01ebff15081d7157a
Ronak <ronakonline1@gmail.com>
2020-02-21 00:46:57 +0530

    DomPDF added in project PDFDownload.php view created for pdf structure

---
+++ application/views/PDFDownload.php
@@ -0,0 +1,105 @@
+<html>
+<head><meta http-equiv=Content-Type content="text/html; charset=UTF-8">
+    <style type="text/css">
+        <!--
+        span.cls_002{font-family:"Matura MT Script Capitals",serif;font-size:14.7px;color:rgb(206,184,102);font-weight:normal;font-style:normal;text-decoration: none}
+        div.cls_002{font-family:"Matura MT Script Capitals",serif;font-size:14.7px;color:rgb(206,184,102);font-weight:normal;font-style:normal;text-decoration: none}
+        span.cls_003{font-family:"Lucida Handwriting Italic",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:normal;font-style:italic;text-decoration: none}
+        div.cls_003{font-family:"Lucida Handwriting Italic",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:normal;font-style:italic;text-decoration: none}
+        span.cls_004{font-family:"Georgia",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        div.cls_004{font-family:"Georgia",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        span.cls_013{font-family:"Georgia",serif;font-size:12.1px;color:rgb(179,51,255);font-weight:normal;font-style:normal;text-decoration: underline}
+        div.cls_013{font-family:"Georgia",serif;font-size:12.1px;color:rgb(179,51,255);font-weight:normal;font-style:normal;text-decoration: none}
+        span.cls_006{font-family:"Georgia",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        div.cls_006{font-family:"Georgia",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        span.cls_009{font-family:"Lucida Handwriting Italic",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:normal;font-style:italic;text-decoration: none}
+        div.cls_009{font-family:"Lucida Handwriting Italic",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:normal;font-style:italic;text-decoration: none}
+        span.cls_010{font-family:"Georgia Bold",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+        div.cls_010{font-family:"Georgia Bold",serif;font-size:12.1px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}
+        span.cls_008{font-family:"Georgia",serif;font-size:10.0px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        div.cls_008{font-family:"Georgia",serif;font-size:10.0px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}
+        span.cls_012{font-family:"Georgia",serif;font-size:14.7px;color:rgb(206,184,102);font-weight:normal;font-style:normal;text-decoration: none}
+        div.cls_012{font-family:"Georgia",serif;font-size:14.7px;color:rgb(206,184,102);font-weight:normal;font-style:normal;text-decoration: none}
+        -->
+    </style>
+    <script type="text/javascript" src="7072e3ea-5413-11ea-8b25-0cc47a792c0a_id_7072e3ea-5413-11ea-8b25-0cc47a792c0a_files/wz_jsgraphics.js"></script>
+</head>
+<body>
+<div style="position:absolute;left:50%;margin-left:-306px;top:0px;width:612px;height:792px;border-style:outset;overflow:hidden">
+    <div style="position:absolute;left:0px;top:0px">
+        <img src="http://design.creativestrokes.xyz/match2marry/assets/images/b1.jpg" width=612 height=792></div>
+    <div style="position:absolute;left:131.18px;top:39.60px" class="cls_002"><span class="cls_002">First Last</span></div>
+    <div style="position:absolute;left:236.93px;top:89.66px" class="cls_003"><span class="cls_003">Who I am ?</span></div>
+    <div style="position:absolute;left:236.93px;top:106.82px" class="cls_004"><span class="cls_004">I consider myself as an honest, motivated indi-</span></div>
+    <div style="position:absolute;left:236.93px;top:122.90px" class="cls_004"><span class="cls_004">vidual, looking for upward mobility in profes-</span></div>
+    <div style="position:absolute;left:236.93px;top:138.98px" class="cls_004"><span class="cls_004">sional career and further academic pursuit.</span></div>
+    <div style="position:absolute;left:236.93px;top:155.06px" class="cls_004"><span class="cls_004">Sensitive to irrational behavior. Positive atti-</span></div>
+    <div style="position:absolute;left:236.93px;top:171.02px" class="cls_004"><span class="cls_004">tude towards life, I believe strategic hard work</span></div>
+    <div style="position:absolute;left:236.93px;top:187.10px" class="cls_004"><span class="cls_004">bring success  in life. Respectful about legiti-</span></div>
+    <div style="position:absolute;left:236.93px;top:203.18px" class="cls_004"><span class="cls_004">mate rights, however, not without responsibil-</span></div>
+    <div style="position:absolute;left:75.74px;top:208.22px" class="cls_004"><span class="cls_004">27 Years (13 August 1992)</span></div>
+    <div style="position:absolute;left:236.93px;top:219.14px" class="cls_004"><span class="cls_004">ity. Just and balanced mannerism, I adore.</span></div>
+    <div style="position:absolute;left:149.54px;top:224.18px" class="cls_004"><span class="cls_004">Raleigh, NC</span></div>
+    <div style="position:absolute;left:139.46px;top:240.29px" class="cls_004"><span class="cls_004">919-543-7687</span></div>
+    <div style="position:absolute;left:95.54px;top:256.25px" class="cls_013"><span class="cls_013"> </span><A HREF="mailto:Firat.Last@gmail.vom">First.Last@gmail.com</A> </div>
+    <div style="position:absolute;left:236.93px;top:268.97px" class="cls_003"><span class="cls_003">Parents</span></div>
+    <div style="position:absolute;left:84.26px;top:272.21px" class="cls_004"><span class="cls_004">Mother Tongue: Bengali</span></div>
+    <div style="position:absolute;left:236.93px;top:285.53px" class="cls_004"><span class="cls_004">Abdul  Karim Jabbar,</span></div>
+    <div style="position:absolute;left:236.93px;top:301.49px" class="cls_004"><span class="cls_004">Ph.D. Electrical Engineering</span></div>
+    <div style="position:absolute;left:236.93px;top:317.45px" class="cls_004"><span class="cls_004">Professor,  EEE,</span></div>
+    <div style="position:absolute;left:236.93px;top:333.53px" class="cls_004"><span class="cls_004">University of Texas , Dallas</span></div>
+    <div style="position:absolute;left:47.52px;top:338.45px" class="cls_003"><span class="cls_003">Overview</span></div>
+    <div style="position:absolute;left:236.93px;top:349.49px" class="cls_004"><span class="cls_004">Shirin Akhtar,</span></div>
+    <div style="position:absolute;left:47.52px;top:354.89px" class="cls_004"><span class="cls_004">Practicing Muslim</span></div>
+    <div style="position:absolute;left:236.93px;top:365.45px" class="cls_004"><span class="cls_004">MSc. Chemistry</span></div>
+    <div style="position:absolute;left:47.52px;top:370.97px" class="cls_004"><span class="cls_004">Design Engineer,</span></div>
+    <div style="position:absolute;left:47.52px;top:386.93px" class="cls_004"><span class="cls_004">Texas Instruments</span></div>
+    <div style="position:absolute;left:47.52px;top:402.87px" class="cls_006"><span class="cls_006">Bachelor EEE</span></div>
+    <div style="position:absolute;left:236.93px;top:408.75px" class="cls_009"><span class="cls_009">Siblings</span></div>
+    <div style="position:absolute;left:47.52px;top:419.71px" class="cls_004"><span class="cls_004">Height 6 feet</span></div>
+    <div style="position:absolute;left:236.93px;top:425.23px" class="cls_004"><span class="cls_004">Abdul Halim  Jabbar,</span></div>
+    <div style="position:absolute;left:47.52px;top:435.67px" class="cls_004"><span class="cls_004">US Citizen by birth</span></div>
+    <div style="position:absolute;left:236.93px;top:441.31px" class="cls_004"><span class="cls_004">Junior,  Computer Science</span></div>
+    <div style="position:absolute;left:236.93px;top:457.27px" class="cls_004"><span class="cls_004">University of Texas , Dallas</span></div>
+    <div style="position:absolute;left:236.93px;top:473.23px" class="cls_004"><span class="cls_004">Suhaila Jabbar,</span></div>
+    <div style="position:absolute;left:49.32px;top:476.23px" class="cls_003"><span class="cls_003">Min. Expectations</span></div>
+    <div style="position:absolute;left:236.93px;top:489.31px" class="cls_004"><span class="cls_004">Junior, Plano East High School</span></div>
+    <div style="position:absolute;left:49.32px;top:492.67px" class="cls_004"><span class="cls_004">Moderate Muslim</span></div>
+    <div style="position:absolute;left:49.32px;top:508.75px" class="cls_004"><span class="cls_004">Any/No profession,</span></div>
+    <div style="position:absolute;left:49.32px;top:524.71px" class="cls_004"><span class="cls_004">Bachelor of Any Discipline</span></div>
+    <div style="position:absolute;left:49.32px;top:541.39px" class="cls_004"><span class="cls_004">Height> 5’-6”</span></div>
+    <div style="position:absolute;left:236.93px;top:543.55px" class="cls_003"><span class="cls_003">Extended Family</span></div>
+    <div style="position:absolute;left:49.32px;top:557.47px" class="cls_004"><span class="cls_004">Non-Immigrant Resident</span></div>
+    <div style="position:absolute;left:236.93px;top:559.99px" class="cls_010"><span class="cls_010">Paternal Side:</span></div>
+    <div style="position:absolute;left:393.67px;top:558.19px" class="cls_010"><span class="cls_010">Maternal Side:</span></div>
+    <div style="position:absolute;left:236.93px;top:578.35px" class="cls_008"><span class="cls_008">Uncle 1: Abdul Hakim Jabbar,</span></div>
+    <div style="position:absolute;left:393.67px;top:576.43px" class="cls_008"><span class="cls_008">Uncle 1: Sohaib Habib,</span></div>
+    <div style="position:absolute;left:236.93px;top:594.34px" class="cls_008"><span class="cls_008">Ph.D. Mechanical Engineering</span></div>
+    <div style="position:absolute;left:393.67px;top:592.54px" class="cls_008"><span class="cls_008">MBA</span></div>
+    <div style="position:absolute;left:236.93px;top:610.30px" class="cls_008"><span class="cls_008">Professor,  EEE,</span></div>
+    <div style="position:absolute;left:393.67px;top:608.50px" class="cls_008"><span class="cls_008">Deputy General Manager</span></div>
+    <div style="position:absolute;left:47.52px;top:612.10px" class="cls_003"><span class="cls_003">Life Style</span></div>
+    <div style="position:absolute;left:236.93px;top:626.38px" class="cls_008"><span class="cls_008">University of Denver,  Colorado</span></div>
+    <div style="position:absolute;left:393.67px;top:624.46px" class="cls_008"><span class="cls_008">Sonali Bank, Dhaka, Bangladesh</span></div>
+    <div style="position:absolute;left:47.52px;top:628.54px" class="cls_004"><span class="cls_004">Extrovert, Friendly, Amicable</span></div>
+    <div style="position:absolute;left:236.93px;top:642.34px" class="cls_008"><span class="cls_008">Aunt 1: Munira Azam</span></div>
+    <div style="position:absolute;left:393.67px;top:640.54px" class="cls_008"><span class="cls_008">Aunt 1: Sohaila Samir</span></div>
+    <div style="position:absolute;left:47.52px;top:644.62px" class="cls_004"><span class="cls_004">Physically Active</span></div>
+    <div style="position:absolute;left:236.93px;top:658.30px" class="cls_008"><span class="cls_008">MSc. Bio-Chemistry</span></div>
+    <div style="position:absolute;left:393.67px;top:656.50px" class="cls_008"><span class="cls_008">MSc. Physics</span></div>
+    <div style="position:absolute;left:47.52px;top:660.58px" class="cls_004"><span class="cls_004">Willing to change for better</span></div>
+    <div style="position:absolute;left:236.93px;top:674.38px" class="cls_008"><span class="cls_008">Home maker</span></div>
+    <div style="position:absolute;left:393.67px;top:672.46px" class="cls_008"><span class="cls_008">Professor, Dhaka College</span></div>
+    <div style="position:absolute;left:47.52px;top:676.54px" class="cls_004"><span class="cls_004">Like Travelling , listening to mu-</span></div>
+    <div style="position:absolute;left:47.52px;top:688.54px" class="cls_004"><span class="cls_004">sic and watching movie</span></div>
+    <div style="position:absolute;left:236.93px;top:689.02px" class="cls_008"><span class="cls_008">Home Maker</span></div>
+    <div style="position:absolute;left:47.52px;top:704.62px" class="cls_004"><span class="cls_004">Ambitious</span></div>
+</div>
+<div style="position:absolute;left:50%;margin-left:-306px;top:802px;width:612px;height:792px;border-style:outset;overflow:hidden">
+    <div style="position:absolute;left:0px;top:0px">
+        <img src="https://design.creativestrokes.xyz/match2marry/assets/images/b1.jpg" width=612 height=792></div>
+    <div style="position:absolute;left:108.98px;top:41.40px" class="cls_012"><span class="cls_012">First Last Name</span></div>
+</div>
+
+</body>
+</html>
